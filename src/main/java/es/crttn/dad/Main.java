469fa079053b68d6dfe901760fa68e91c0f6f62b
crttn <crttngr@gmail.com>
2024-10-10 13:47:29 +0100

    Initial Commit

---
+++ src/main/java/es/crttn/dad/Main.java
@@ -0,0 +1,15 @@
+package es.crttn.dad;
+
+
+import javafx.application.Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args ) {
+
+        Application.launch(MainController.class, args);
+    }
+}
---
+++ src/main/java/es/crttn/dad/MainController.java
@@ -0,0 +1,15 @@
+package es.crttn.dad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+
+    }
+}
